9d513ceebe7f54fc823d8f6c304e54a2f7b86b4a
Ahmed Wakeel <elwakeel.ah@gmail.com>
2026-05-02 11:20:34 +0200

    Refactor user-data.sh to streamline installation steps and ensure consistent application management with pm2

--- user-data.sh
+++ user-data.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
+set -e
 
-# Update package lists
 apt-get update -y
+apt-get install -y git nodejs npm
 
-# Install git
-apt-get install git -y
-
-# Clone your repository (replace 'https://your_github_repo.git' with your actual repo URL)
+rm -rf /var/www/app
 git clone https://github.com/Elwakeel-ah/EC2-b.git /var/www/app
-
-# update
-sudo apt update
-
-# Install Node.js and npm using nvm (Node Version Manager)
-sudo apt install -y nodejs
-
-sudo apt install -y npm
 
 cd /var/www/app
 
 npm install
 
-# Navigate to the application directory
-
-# Install application dependencies
 npm install -g pm2
-pm2 start main.js --name app-a
+
+pm2 delete app-b || true
+pm2 start main.js --name app-b
 pm2 startup systemd -u root --hp /root
 pm2 save
